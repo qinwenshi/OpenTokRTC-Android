7686b2e64d864e10458597e6b0f67f84aa57b2b5
Ankur Oberoi <aoberoi@gmail.com>
2013-11-10 18:48:52 -0500

    generates ChatRoomActivity

---
+++ OpenTokRTC/src/main/java/com/tokbox/android/opentokrtc/ChatRoomActivity.java
@@ -0,0 +1,14 @@
+package com.tokbox.android.opentokrtc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ankur on 11/10/13.
+ */
+public class ChatRoomActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_room);
+    }
+}
