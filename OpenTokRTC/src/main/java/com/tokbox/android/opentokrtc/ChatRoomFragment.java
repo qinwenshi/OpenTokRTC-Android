2e5103dbd8baf2f93c81e689276c358a20adf2b6
Ankur Oberoi <aoberoi@gmail.com>
2013-11-11 07:02:07 -0500

    making room name a member of chat room fragment

--- OpenTokRTC/src/main/java/com/tokbox/android/opentokrtc/ChatRoomFragment.java
+++ OpenTokRTC/src/main/java/com/tokbox/android/opentokrtc/ChatRoomFragment.java
@@ -15,6 +15,8 @@ public class ChatRoomFragment extends Fragment {
     public static final String ARG_ROOM_ID = "room_id";
     public static final String TAG = "ChatRoomFragment";
 
+    private String mRoomName;
+
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
@@ -27,15 +29,31 @@ public class ChatRoomFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         String roomName = getArguments().getString(ARG_ROOM_ID);
-        if (roomName != null) {
-            // TODO: initialize everything
-            Log.i(TAG, "initializing chat room fragment for room: " + roomName);
-            getActivity().setTitle(roomName);
-        }
+        setRoomName(roomName);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_chat_room, container, false);
+    }
+
+
+    public void setRoomName(String roomName) {
+        if (roomName != null && !roomName.equals(mRoomName)) {
+            mRoomName = roomName;
+            initializeRoom();
+        }
+    }
+
+    public String getRoomName() {
+        return mRoomName;
+    }
+
+    /**
+     * Initialization occurs as soon as a new roomName has been set
+     */
+    private void initializeRoom() {
+        Log.i(TAG, "initializing chat room fragment for room: " + mRoomName);
+        getActivity().setTitle(mRoomName);
     }
 }
